210ff381d919da99b9716fa607daf6f1383b9e5e
Adams2303 <sadekadam2303@gmail.com>
2023-07-11 19:41:34 +0100

    update

--- src/ReactProject/About.jsx
+++ src/ReactProject/About.jsx
@@ -29,14 +29,14 @@ const AboutSection = () => {
   };
   return (
     <section
-      id='biography'
+      id="biography"
       style={{ backgroundColor: "#0e0e17", padding: "100px 0", color: "white" }}
     >
       <Container>
-        <Row className='align-items-center justify-content-center'>
+        <Row className="align-items-center justify-content-center">
           <Col md={6} lg={4}>
             <div
-              className='info'
+              className="info"
               style={{
                 border: "5px solid cyan",
                 borderRadius: "50%",
@@ -47,8 +47,8 @@ const AboutSection = () => {
               }}
             >
               <img
-                src='https://ddgimgs-f43e.kxcdn.com/1961602/cwqq9m_39ea89a21dae436c5ce4ccfd287ad4045dde19c1.jpg'
-                alt='Random Male Face'
+                src="https://ddgimgs-f43e.kxcdn.com/1961602/cwqq9m_39ea89a21dae436c5ce4ccfd287ad4045dde19c1.jpg"
+                alt="Random Male Face"
                 style={{
                   height: "100%",
                   width: "100%",
@@ -66,7 +66,7 @@ const AboutSection = () => {
                 }}
               >
                 <a
-                  href='https://facebook.com'
+                  href="https://facebook.com"
                   style={{
                     border: "2px solid #fff",
                     borderRadius: "50%",
@@ -79,59 +79,59 @@ const AboutSection = () => {
                 >
                   <FontAwesomeIcon
                     icon={faFacebook}
-                    size='lg'
-                    className='mx-2'
-                    color='#fff'
-                    backgroundColor='#0b0b13'
+                    size="lg"
+                    className="mx-2"
+                    color="#fff"
+                    backgroundColor="#0b0b13"
                   />
                 </a>
-                <a href='https://twitter.com'>
+                <a href="https://twitter.com">
                   <FontAwesomeIcon
                     icon={faTwitter}
-                    size='lg'
-                    className='mx-2'
-                    color='#fff'
-                    backgroundColor='#0b0b13'
+                    size="lg"
+                    className="mx-2"
+                    color="#fff"
+                    backgroundColor="#0b0b13"
                   />
                 </a>
-                <a href='https://instagram.com'>
+                <a href="https://instagram.com">
                   <FontAwesomeIcon
                     icon={faInstagram}
-                    size='lg'
-                    className='mx-2'
-                    color='#fff'
-                    backgroundColor='#0b0b13'
+                    size="lg"
+                    className="mx-2"
+                    color="#fff"
+                    backgroundColor="#0b0b13"
                   />
                 </a>
-                <a href='https://linkedin.com'>
+                <a href="https://linkedin.com">
                   <FontAwesomeIcon
                     icon={faLinkedin}
-                    size='lg'
-                    className='mx-2'
-                    color='#fff'
-                    backgroundColor='#0b0b13'
+                    size="lg"
+                    className="mx-2"
+                    color="#fff"
+                    backgroundColor="#0b0b13"
                   />
                 </a>
-                <a href='https://pinterest.com'>
+                <a href="https://pinterest.com">
                   <FontAwesomeIcon
                     icon={faPinterest}
-                    size='lg'
-                    className='mx-2'
-                    color='#fff'
-                    backgroundColor='#0b0b13'
+                    size="lg"
+                    className="mx-2"
+                    color="#fff"
+                    backgroundColor="#0b0b13"
                   />
                 </a>
               </div>
             </div>
-            <p className='text-center mt-4'>Web Development Student</p>
-            <h3 className='text-center'>Adam Sadek</h3>
+            <p className="text-center mt-4">Web Development Student</p>
+            <h3 className="text-center">Adam Sadek</h3>
           </Col>
-          <Col lg={7} className='ml-auto '>
+          <Col lg={7} className="ml-auto ">
             <div>
-              <h3 className='title'>Biography</h3>
-              <div className='bio'>
+              <h3 className="title">Biography</h3>
+              <div className="bio">
                 <p>
-                  Hello, my name is Adam Sadek and I am a web development
+                  Hello, my name are Adam Sadek and I am a web development
                   student. I am passionate about learning new technologies and
                   applying them to create amazing web experiences for users.
                 </p>
@@ -142,8 +142,8 @@ const AboutSection = () => {
                   development.
                 </p>
               </div>
-              <div className='row info-list'>
-                <div className='col-sm-6'>
+              <div className="row info-list">
+                <div className="col-sm-6">
                   <ul>
                     <li>
                       <label>Age:</label> <span>22</span>
@@ -160,7 +160,7 @@ const AboutSection = () => {
                     </li>
                   </ul>
                 </div>
-                <div className='col-sm-6'>
+                <div className="col-sm-6">
                   <ul>
                     <li>
                       <label>Website:</label> <span>www.adamsadek.com</span>
@@ -181,24 +181,24 @@ const AboutSection = () => {
           </Col>
         </Row>
         <div
-          className='seperated'
+          className="seperated"
           style={{
             backgroundImage:
               "url( 'https://nairo.ibthemespro.com/img/border-dark.png')",
           }}
         ></div>
-        <div className='title'>
+        <div className="title">
           <h3>My Specialities</h3>
         </div>
         <Row>
-          <Col md={6} lg={4} className='my-3'>
-            <Card bg='black' text='white' className='my-card'>
+          <Col md={6} lg={4} className="my-3">
+            <Card bg="black" text="white" className="my-card">
               <CardHeader>
-                <FontAwesomeIcon className='icon' icon={faUserGraduate} />
+                <FontAwesomeIcon className="icon" icon={faUserGraduate} />
               </CardHeader>
-              <Card.Body className='card-body'>
-                <Card.Title className='title'> Humanities </Card.Title>
-                <Card.Text className='text'>
+              <Card.Body className="card-body">
+                <Card.Title className="title"> Humanities </Card.Title>
+                <Card.Text className="text">
                   In an increasingly Globalized World, A foundation in
                   humanities furnishes a web developer with the ability to
                   comprehend and effectively communicate with diverse audiences,
@@ -207,14 +207,14 @@ const AboutSection = () => {
               </Card.Body>
             </Card>
           </Col>
-          <Col md={6} lg={4} className='my-3'>
-            <Card bg='black' text='white' className='my-card'>
+          <Col md={6} lg={4} className="my-3">
+            <Card bg="black" text="white" className="my-card">
               <CardHeader>
-                <FontAwesomeIcon className='icon' icon={faWandMagicSparkles} />
+                <FontAwesomeIcon className="icon" icon={faWandMagicSparkles} />
               </CardHeader>
-              <Card.Body className='card-body'>
-                <Card.Title className='title'> Web Design</Card.Title>
-                <Card.Text className='text'>
+              <Card.Body className="card-body">
+                <Card.Title className="title"> Web Design</Card.Title>
+                <Card.Text className="text">
                   A comprehensive understanding of web design philosophy
                   empowers a web developer to design visually appealing and
                   user-friendly websites that effectively convey intended
@@ -223,14 +223,14 @@ const AboutSection = () => {
               </Card.Body>
             </Card>
           </Col>
-          <Col md={6} lg={4} className='my-3'>
-            <Card bg='black' text='white' className='my-card'>
+          <Col md={6} lg={4} className="my-3">
+            <Card bg="black" text="white" className="my-card">
               <CardHeader>
-                <FontAwesomeIcon className='icon' icon={faCode} />
+                <FontAwesomeIcon className="icon" icon={faCode} />
               </CardHeader>
-              <Card.Body className='card-body'>
-                <Card.Title className='title'> Web Development</Card.Title>
-                <Card.Text className='text'>
+              <Card.Body className="card-body">
+                <Card.Title className="title"> Web Development</Card.Title>
+                <Card.Text className="text">
                   In the current digital era, web development is a pivotal
                   discipline that facilitates the creation and dissemination of
                   information, goods, and services via the internet by
@@ -241,16 +241,16 @@ const AboutSection = () => {
           </Col>
         </Row>
         <div
-          className='seperated'
+          className="seperated"
           style={{
             backgroundImage:
               "url( 'https://nairo.ibthemespro.com/img/border-dark.png')",
           }}
         ></div>
-        <div className='title'>
+        <div className="title">
           <h3>Testimonials.</h3>
         </div>
-        <div className='testimonials-container'>
+        <div className="testimonials-container">
           <div>
             <h2> Single Item</h2>
             <Slider {...settings}>
